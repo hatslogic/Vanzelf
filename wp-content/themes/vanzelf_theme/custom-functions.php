6f916d60c7fcf0d990068c7db11e4ce249a49955
Saniya Claudia <sania@2hatslogic.com>
2021-05-28 12:01:20 +0530

    custom-functions.php setup added two new custom stylesheets

--- wp-content/themes/vanzelf_theme/custom-functions.php
+++ wp-content/themes/vanzelf_theme/custom-functions.php
@@ -1,1 +1,11 @@
 <?php 
+
+function add_theme_styles()
+				{
+					//For style
+                    $version = wp_get_theme()->get('Version');
+					wp_enqueue_style('masterstyle', get_template_directory_uri() . '/dist/styles/master.css', array(), $version, 'all');
+                    wp_enqueue_style('menustyle', get_template_directory_uri() . '/dist/styles/stylemenu.css', array(), $version, 'all');
+                    
+                }
+                add_action('wp_enqueue_scripts', 'add_theme_styles');
